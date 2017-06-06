01e0c86958e6ed14e94b8516d64d9865d3b1b083
Lucero07 <ing.lucero.ramos@gmail.com>
2017-06-06 01:57:15 -0500

    funcionalidad en localizacion

--- js/app.js
+++ js/app.js
@@ -33,7 +33,7 @@
       "aria-label": "Close"
     });
     var $tarjeta = $("<div />", {
-      "class": "panel panel-group"
+      "class": "panel panel-group restaurantesGarnacha"
     });
     var $encabezadoTarjeta = $("<div />", {
       "class": "panel-heading",
@@ -50,32 +50,42 @@
       "class": "panel-body"
     });
     var $enlaceDatos = $("<a />");
+    var $botonGarnacha = $("<button />",{
+      "type":"button",
+      "class":"btn btn-success btn-large"
+    });
 
     var id = "marcador-" + contador;
     var idcuerpoTarjeta = "collapse-" + contador;
 
 
-    // Personalizamos elementos
 
+    // Personalizamos elementos
     $eliminar.id = id;
     $nombreTitulo.attr("for", id);
     $cuerpoTarjeta.id = "tituloTarjeta" + contador;
     $enlaceDatos.id = "enlaceDatos" + contador;
     $enlaceTitulo.attr("aria-controls", idcuerpoTarjeta);
+    $botonGarnacha.id = "get-location" + contador;
     $eliminar.click(borrarRestauante);
     $enlaceTitulo.click(colapso);
+    $botonGarnacha.click(obtenerUbicacion);
+
 
     // Agregarlos al DOM
     $enlaceDatos.append(recomendaciones); // no lo inserta :(
     $datosTarjeta.html("Dirección: " + direccionRestauante + "<br> " + "Recomendación: " + recomendaciones);
     $cuerpoTarjeta.append($datosTarjeta);
-    $enlaceTitulo.html( + nombreRestauante + "<br> " + "Contacto" + numeroRestauante);
+    $enlaceTitulo.html(" Nombre: " + nombreRestauante + "<br> " + "Contacto: " + numeroRestauante);
     $tituloTarjeta.append($enlaceTitulo);
     $encabezadoTarjeta.append($tituloTarjeta);
     $tarjeta.append($encabezadoTarjeta, $cuerpoTarjeta);
     $acordion.append($tarjeta);
     $eliminar.append($equis);
+    $botonGarnacha.text("Obtener Ubicacion");
     $tarjeta.append($eliminar);
+    $tarjeta.append($botonGarnacha);
+
 
 
     //Borrar contenido de textarea
@@ -84,6 +94,11 @@
     $direccion.val("");
     $recomendaciones.val("");
     $botonRestauante.attr("disabled", true);
+    //Mardar llamar el mapa
+    $botonGarnacha.click(obtenerUbicacion);
+    var restaurantes= [];
+     restaurantes.push($tarjeta);
+     console.log(restaurantes);
 
     contador++;
   };
@@ -110,10 +125,87 @@
       $tarjetaEsconder.hide();
     } else {
       $tarjetaEsconder.show();
-      $tarjetaEsconder.css("background-color", "#8b33a1");
-      $tarjetaEsconder.css("color", "#ebd8dd");
+      $tarjetaEsconder.css("background-color", "#56f159");
+      $tarjetaEsconder.css("color", "#060517");
     }
   };
+  //para funcionalidad de geolocalizacion
+
+
+
+var obtenerUbicacion = function(event) {
+if (navigator.geolocation) {
+  navigator.geolocation.getCurrentPosition(mostrarPosicion);
+} else {
+  alert("Actualice su navegador");
+}
+};
+
+var mostrarPosicion = function (posicion) {
+var coordenadas = {
+  lat: posicion.coords.latitude,
+  lng: posicion.coords.longitude
+};
+mostrarMapa(coordenadas);
+};
+
+var mostrarMapa = function (coordenadas) {
+  var map = new google.maps.Map($('#map')[0], {
+    zoom: 17,
+    center: coordenadas
+  });
+  var marker = new google.maps.Marker({
+    position: coordenadas,
+    map: map
+  });
+};
+  $("#ubicacion").click(obtenerUbicacion);
+
+// funcion para buscar.
+$("#formularioBuscar").change(filtrarContactos);
+   var filtrarContactos = function (e) {
+  	e.preventDefault();
+    var restaurantesGarnacha = $(".restaurantesGarnacha");
+ 	  var criterioBusqueda = $("#buscarRestaurante").val().toLowerCase();
+  	var contactosFiltrados = restaurantesGarnacha.filter(function (contacto) {
+  		return restaurantesGarnacha.child.child.child.toLowerCase().indexOf(criterioBusqueda) >= 0;
+        console.log(contactosFiltrados);
+  });
+  console.log(contactosFiltrados);
+  // 	//mostrarContactos(contactosFiltrados);
+   };
+
+
+
+   var directionsDisplay = new google.maps.DirectionsRenderer();
+   var directionsService = new google.maps.DirectionsService();
+
+
+   var request = {
+    origin: $('#origen').val(),
+    destination: $('#destino').val(),
+    travelMode: google.maps.DirectionsTravelMode[$('#modo_viaje').val()],
+    unitSystem: google.maps.DirectionsUnitSystem[$('#tipo_sistema').val()],
+    provideRouteAlternatives: true
+    };
+
+    directionsService.route(request, function(response, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setMap(map);
+            directionsDisplay.setPanel($("#panel_ruta").get(0));
+            directionsDisplay.setDirections(response);
+        } else {
+                alert("No existen rutas entre ambos puntos");
+        }
+    });
+    $('#buscar').live('click', function(){
+    rockAndRoll();
+});
+
+$('.opciones_ruta').live('change', function(){
+    rockAndRoll();
+});
+
 
 
 
@@ -122,11 +214,3 @@
   // Cuando carga la página
   $(document).ready(crearTarjetaRestauante);
 })();
-function initMap() {
-        // Create a map object and specify the DOM element for display.
-        var map = new google.maps.Map(document.getElementById('map'), {
-          center: {lat: -34.397, lng: 150.644},
-          scrollwheel: false,
-          zoom: 8
-        });
-      }
